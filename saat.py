e58ffcb8e6bb4a35b6bf645e8bbdf1b1b1d68beb
Ebrar Kaya <ebrkaya9852@gmail.com>
2025-03-07 22:16:07 +0300

    Update saat.py

--- saat.py
+++ saat.py
@@ -1,9 +1,7 @@
 import pygame
 import math
 from datetime import datetime
-
 pygame.init()
-
 GENISLIK, YUKSEKLIK = 760, 760
 pencere = pygame.display.set_mode((GENISLIK, YUKSEKLIK))
 saat1 = pygame.image.load("saat1.png")
@@ -16,7 +14,7 @@ saat4 = pygame.image.load("saat4.png")
 saat4 = pygame.transform.scale(saat4, (GENISLIK, YUKSEKLIK))
 saat5 = pygame.image.load("saat5.png")
 saat5 = pygame.transform.scale(saat5, (GENISLIK, YUKSEKLIK))
-x = 0  # Global index for clock face
+x = 0 
 kadranlar = [saat1, saat2, saat3, saat4, saat5]
 class Saat:
     def __init__(self):
@@ -24,13 +22,11 @@ class Saat:
         self.akrep_uzunluk = 150
         self.yelkovan_uzunluk = 200
         self.saniye_uzunluk = 220
-    
     def cizgi_ciz(self, aci, uzunluk, renk, kalinlik):
         radians = math.radians(aci - 90)
         x = self.merkezx + uzunluk * math.cos(radians)
         y = self.merkezy + uzunluk * math.sin(radians)
         pygame.draw.line(pencere, renk, (self.merkezx, self.merkezy), (x, y), kalinlik)
-
     def guncelle(self):
         saat = datetime.now().hour % 12
         dakika = datetime.now().minute
@@ -43,22 +39,20 @@ class Saat:
         self.cizgi_ciz(akrep_aci, self.akrep_uzunluk, (0, 0, 0), 6)
         self.cizgi_ciz(yelkovan_aci, self.yelkovan_uzunluk, (0, 0, 0), 6)
         self.cizgi_ciz(saniye_aci, self.saniye_uzunluk, (0, 0, 0), 3)
-    
     def kadrandegistir(self):
         global x
         x += 1
         if x > 4:
             x = 0
         pencere.blit(kadranlar[x], (0,0))
-
 saat = Saat()
 durum = True
 while durum:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for ebrar in pygame.event.get():
+        if ebrar.type == pygame.QUIT:
             durum = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
+        elif ebrar.type == pygame.KEYDOWN:
+            if ebrar.key == pygame.K_RIGHT:
                 saat.kadrandegistir()
     pencere.blit(kadranlar[x], (0,0))
     saat.guncelle()
